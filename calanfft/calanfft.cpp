4ef34a2e7d339356e7939696de9e3d6f8619da93
Franco Curotto <francocurotto@gmail.com>
2020-06-18 15:33:44 -0400

    testing

--- calanfft/calanfft.cpp
+++ calanfft/calanfft.cpp
@@ -8,10 +8,11 @@ void fft_stage(int stage,
 
     twiddles_loop: for(int i=0; i<m/2; i++) {
     	wtype wr; wtype wi;
-        get_twiddles1(n, i, &wr, &wi);
+    	get_twiddles1(n, i, &wr, &wi);
 
-		btfly_loop: for(int j=0; j<n; j++) {
+    	btfly_loop: for(int j=0; j<n; j++) {
         	#pragma HLS PIPELINE
+
 			int k1 = j*m + i;  // index of btfly 1st input
         	int k2 = k1 + m/2; // index of btfly 2nd input
             btfly2(xr[k1], xi[k1], xr[k2], xi[k2], wr, wi,
@@ -26,26 +27,12 @@ void calanfft(dtype xr[SIZE], dtype xi[SIZE],
 	#pragma HLS RESOURCE variable=xi core=RAM_1P
 	#pragma HLS RESOURCE variable=yr core=RAM_1P
 	#pragma HLS RESOURCE variable=yi core=RAM_1P
-	//#pragma HLS INTERFACE ap_fifo port=xr
-	//#pragma HLS INTERFACE ap_fifo port=xi
-	//#pragma HLS INTERFACE ap_fifo port=yr
-	//#pragma HLS INTERFACE ap_fifo port=yi
-	//#pragma HLS STREAM variable=xr dim=1
-	//#pragma HLS STREAM variable=xi dim=1
-	//#pragma HLS STREAM variable=yr dim=1
-	//#pragma HLS STREAM variable=yi dim=1
 	#pragma HLS DATAFLOW
 
     // array that contains the fft results in 
 	// intermediate stage
     dtype arr_r[STAGES-1][SIZE];
     dtype arr_i[STAGES-1][SIZE];
-	#pragma HLS RESOURCE variable=arr_r core=RAM_1P
-	#pragma HLS RESOURCE variable=arr_i core=RAM_1P
-	//#pragma HLS RESOURCE variable=arr_r core=FIFO
-	//#pragma HLS RESOURCE variable=arr_i core=FIFO
-	//#pragma HLS STREAM variable=arr_r dim=2
-	//#pragma HLS STREAM variable=arr_i dim=2
 	#pragma HLS ARRAY_PARTITION variable=arr_r complete dim=1
 	#pragma HLS ARRAY_PARTITION variable=arr_i complete dim=1
 
@@ -54,7 +41,7 @@ void calanfft(dtype xr[SIZE], dtype xi[SIZE],
 
     // do intermediate stages
     stage_loop: for(int s=1; s<=STAGES-2; s++) {
-	#pragma HLS UNROLL
+		#pragma HLS UNROLL
     	fft_stage(s, arr_r[s-1], arr_i[s-1],
                      arr_r[s],   arr_i[s]);
     }
